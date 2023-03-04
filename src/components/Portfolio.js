9db39c9292fda7d4cceab7aafe5621cfc40836e6
Ivan Barrios <ivanbar8112003@gmail.com>
2023-03-04 11:14:20 -0300

    Portfolio skeleton

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,32 +1,46 @@
 import "./styles/portfolio.css";
 
 const Portfolio = () => {
+  let projects = [
+    {
+      name: "Project 1",
+      imgsrc: "#",
+      describe:
+        "The project 1 do this, this and this. The main goal was doing this.",
+    },
+    {
+      name: "Project 2",
+      imgsrc: "#",
+      describe:
+        "The project 2 do this, this and this. The main goal was doing this.",
+    },
+    {
+      name: "Project 3",
+      imgsrc: "#",
+      describe:
+        "The project 3 do this, this and this. The main goal was doing this.",
+    },
+    {
+      name: "Project 4",
+      imgsrc: "#",
+      describe:
+        "The project 4 do this, this and this. The main goal was doing this.",
+    },
+  ];
+
   return (
-    <div>
+    <div className="portfolio">
       <h1>Portfolio</h1>
-      <div>
-        <div>
-          <img src="#" alt="Project" />
-        </div>
-        <div>Description</div>
-      </div>
-      <div>
-        <div>
-          <img src="#" alt="Project" />
-        </div>
-        <div>Description</div>
-      </div>
-      <div>
-        <div>
-          <img src="#" alt="Project" />
-        </div>
-        <div>Description</div>
-      </div>
-      <div>
-        <div>
-          <img src="#" alt="Project" />
-        </div>
-        <div>Description</div>
+      <div className="projects">
+        {projects.map((project) => (
+          <div className="project">
+            <div className="project-img-container">
+              <img src={project.imgsrc} alt={project.name} />
+            </div>
+            <h2>{project.name}</h2>
+            <p>{project.describe}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
