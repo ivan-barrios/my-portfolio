f128e347f5e7c8638d95eefb285e95f3fcc095dc
Ivan Barrios <ivanbar8112003@gmail.com>
2023-03-05 17:45:39 -0300

    start with dark mode

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export const ThemeContext = createContext(null);
 
 function App() {
   const [showMenu, setShowMenu] = useState(false);
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
